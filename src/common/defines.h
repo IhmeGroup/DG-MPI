e07e8043d83401b5474541a5689bb1b11ee932de
alilasemi <alasemi@stanford.edu>
2022-01-17 11:20:22 -0800

    Add defines and exceptions

---
+++ src/common/defines.h
@@ -0,0 +1,39 @@
+#ifndef DG_DEFINES_H
+#define DG_DEFINES_H
+
+// compiler directive to switch between single and double precision
+#ifdef USE_DOUBLES
+using rtype = double;
+#else
+using rtype = float;
+#endif
+
+#define INIT_EX_PARAMS_MAX 10
+
+#define N_BASIS_MAX 300  //!< maximum number of basis function
+#define N_STATE_MAX 20 //!< maximum number of state variables
+#define N_COEFFS_MAX (N_BASIS_MAX * N_STATE_MAX) //!< maximum number of solution coefficients (ns*nb)
+#define N_FACE_MAX 8 //!< maximum number of faces (8 = hexahedron)
+#define N_BFG_MAX 5 //!< maximum number of boundary face groups
+#define N_BDATA_MAX 10 //!< maximum number of boundary data per boundary face group
+#define N_VAR_OUTPUT_MAX 40 //!< maximum number of variables for output, diagnosis etc.
+#define N_VAR_BDIAG_MAX 10 //!< maximum number of variables for boundary diagnosis, less than N_VAR_OUTPUT_MAX because of Legion limit on return type's size
+
+#define FILE_NAME_LEN_MAX 128 //!< maximum length of a file name
+#define VAR_NAME_LEN_MAX 128 //!< maximum length of a variable name
+
+#ifdef DG_USE_KOKKOS
+/* It is the responsibility of the file using Kokkos to include the Kokkos headers.
+ * Namely, include <Kokkos_Core.hpp> guarded by DG_USE_KOKKOS
+ * for the following macros to be defined. */
+#define DG_KOKKOS_FUNCTION KOKKOS_FUNCTION
+#define DG_KOKKOS_INLINE_FUNCTION KOKKOS_INLINE_FUNCTION
+#define DG_KOKKOS_FORCEINLINE_FUNCTION KOKKOS_FORCEINLINE_FUNCTION
+#else
+/* If Kokkos is not used, then these macros are empty which ensures backward compatibility. */
+#define DG_KOKKOS_FUNCTION
+#define DG_KOKKOS_INLINE_FUNCTION inline
+#define DG_KOKKOS_FORCEINLINE_FUNCTION inline
+#endif
+
+#endif //DG_DEFINES_H
---
+++ src/common/my_exceptions.h
@@ -0,0 +1,101 @@
+//
+// Created by kihiro on 2/19/20.
+//
+
+#ifndef DG_MY_EXCEPTIONS_H
+#define DG_MY_EXCEPTIONS_H
+
+#include <exception>
+#include <string>
+
+/*! \brief Exception for incompatible or incomplete input file information
+ *
+ */
+class InputException : public std::exception {
+  public:
+    InputException(std::string msg_) : msg(msg_) {}
+
+    const char *what() const throw() {
+        return msg.c_str();
+    }
+
+  private:
+    std::string msg;
+};
+
+/*! \brief Exception for non-physical results
+ *
+ */
+class NotPhysicalException : public std::exception {
+  public:
+    NotPhysicalException(std::string msg_) : msg(msg_) {}
+
+    const char * what () const throw () {
+        return msg.c_str();
+    }
+
+  private:
+    std::string msg;
+};
+
+/*! \brief Exception for not implemented methods
+ *
+ */
+class NotImplementedException : public std::exception {
+  public:
+    NotImplementedException(std::string msg_) : msg(msg_) {}
+
+    const char *what() const throw() {
+        return msg.c_str();
+    }
+
+  private:
+    std::string msg;
+};
+
+/*! \brief Exception for failed system calls
+ *
+ */
+class SystemCallException : public std::exception {
+  public:
+    SystemCallException(std::string msg_) : msg(msg_) {}
+
+    const char *what() const throw() {
+        return msg.c_str();
+    }
+
+  private:
+    std::string msg;
+};
+
+/*! \brief Exception for fatal errors that prevent from continuing safely
+ *
+ */
+class FatalException : public std::exception {
+  public:
+    FatalException(std::string msg_) : msg(msg_) {}
+
+    const char *what() const throw() {
+        return msg.c_str();
+    }
+
+  private:
+    std::string msg;
+};
+
+/*! \brief Exception for failed convergence in an iterative procedure
+ *
+ */
+class FailedConvergenceException : public std::exception {
+  public:
+    FailedConvergenceException(std::string msg_) : msg(msg_) {}
+
+    const char *what() const throw() {
+        return msg.c_str();
+    }
+
+  private:
+    std::string msg;
+};
+
+#endif //DG_MY_EXCEPTIONS_H
