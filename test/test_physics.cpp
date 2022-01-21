f2de6a995921e0c0373ce06329254430804d75bd
Brett Bornhoft <bbornhoft@gmail.com>
2022-01-20 16:17:50 -0800

    remove some simple dumb test for classes

--- test/test_physics.cpp
+++ test/test_physics.cpp
@@ -11,9 +11,9 @@
 // #include "equations/navier_stokes.h"
 // #include "equations/navier_stokes_multispecies.h"
 
-using namespace std;
-// using namespace Physics;
-// using namespace Equation;
+#include <Kokkos_Core.hpp>
+
+
 // using VectorType = Eigen::Matrix<rtype, Eigen::Dynamic, 1>;
 
 constexpr rtype DOUBLE_TOL = 1E-13;
@@ -26,26 +26,30 @@ TEST(PhysicsTestSuite, ConvFluxPhysical) {
     const rtype gam = 1.4;
     const rtype rhoE = P / (gam - 1.) + 0.5 * rho * (u * u + v * v);
 
-    rtype U[4];
-    U[0] = rho;
-    U[1] = rho * u;
-    U[2] = rho * v;
-    U[3] = P / (gam - 1.) + 0.5 * rho * (u * u + v * v);
+    Kokkos::initialize();
 
-    rtype Fref[8];
-    Fref[0] = rho * u;
-    Fref[1] = rho * u * u + P;
-    Fref[2] = rho * u * v;
-    Fref[3] = (rhoE + P) * u;
-    Fref[4] = rho * v;
-    Fref[5] = rho * u * v;
-    Fref[6] = rho * v * v + P;
-    Fref[7] = (rhoE + P) * v;
+    Kokkos::View<rtype[4]> U("U");
+    U(0) = rho;
+    U(1) = rho * u;
+    U(2) = rho * v;
+    U(3) = P / (gam - 1.) + 0.5 * rho * (u * u + v * v);
 
-    rtype F[8];
-    Physics::EulerBase<2>::conv_flux_physical(4, U, P, F);
+    Kokkos::View<rtype[4][2]> Fref("Fref");
+    Fref(0, 0) = rho * u;
+    Fref(1, 0) = rho * u * u + P;
+    Fref(2, 0) = rho * u * v;
+    Fref(3, 0) = (rhoE + P) * u;
+    Fref(0, 1) = rho * v;
+    Fref(1, 1) = rho * u * v;
+    Fref(2, 1) = rho * v * v + P;
+    Fref(3, 1) = (rhoE + P) * v;
 
-    for (unsigned i = 0; i < 8; i++) {
-        EXPECT_NEAR(F[i], Fref[i], DOUBLE_TOL);
+    Kokkos::View<rtype[4][2]> F("F");
+    Physics::EulerBase<2>::conv_flux_physical(U, P, F);
+  
+    for (unsigned i = 0; i < 4; i++) {
+        for (unsigned j = 0; j < 2; j++){
+            EXPECT_NEAR(F(i, j), Fref(i, j), DOUBLE_TOL);
+        }
     }
 }
