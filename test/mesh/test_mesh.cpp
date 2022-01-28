b47705048e0e1534c5fe515a5fa855e429915f36
alilasemi <alasemi@stanford.edu>
2022-01-28 12:08:18 -0800

    Undo Mesh test, issues with MPI testing
    Trying to use MPI with googletest just creates N process each with one
    rank, as opposed to making one process with N ranks. No idea why this
    happens, may be a googletest thread safety issue.

--- test/mesh/test_mesh.cpp
+++ test/mesh/test_mesh.cpp
@@ -3,17 +3,27 @@
 #include <iostream>
 #include "memory/memory_network.h"
 #include "mesh/mesh.h"
+#include <filesystem>
 
 using std::string;
 
+namespace fs = std::filesystem;
+
 
 TEST(MeshTestSuite, ShouldPartitionFourQuadsInHalfWithTwoRanks) {
-    //// Read input file name
-    //string toml_fname = "input.toml";
-    //auto toml_input = toml::parse(toml_fname);
-    //const int dim = toml::find<int>(toml_input, "Physics", "dim");
+    // TODO
+    int argc = 1;
+    char** argv;
+    //MemoryNetwork network(argc, argv);
+    // TODO: Get project directory instead
+    string prefix = "../test/mesh/";
 
-    //// Create mesh
+    // Read input file name
+    string toml_fname = prefix + "input.toml";
+    auto toml_input = toml::parse(toml_fname);
+    const int dim = toml::find<int>(toml_input, "Physics", "dim");
+
+    // Create mesh
     //auto mesh = Mesh(toml_input, network);
     EXPECT_STRNE("hello", "world");
 }
