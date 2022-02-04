e1fd8192ac3e21350e34e09e9ddfdd1c938c915f
Brett Bornhoft <bbornhoft@gmail.com>
2022-02-04 11:53:32 -0800

    make one node an option for getting equidistant nodes" "

--- src/numerics/basis/tools.cpp
+++ src/numerics/basis/tools.cpp
@@ -7,12 +7,13 @@ void equidistant_nodes_1D_range(rtype start, rtype stop, int nnodes,
 	Kokkos::View<rtype*> &xnodes) {
 
 	if (nnodes <= 1){
-		throw ValueErrorException("Need at least two nodes to compute");
+		xnodes(0) = 0.;
+		return;
 	}
 	if (stop <= start) {
 		throw ValueErrorException("Assume beginning is smaller than end");
 	}
-	if (xnodes.extent (0) != nnodes){
+	if (xnodes.extent(0) != nnodes){
 		Kokkos::resize(xnodes, nnodes);
 	}
 
--- test/numerics/basis/test_tools.cpp
+++ test/numerics/basis/test_tools.cpp
@@ -5,6 +5,22 @@
 
 constexpr rtype DOUBLE_TOL = 1E-13;
 
+
+/*
+Tests that one point returns a zero size 
+*/
+TEST(basis_tools_test_suite, test_1D_nodes_one_point){
+	
+	Kokkos::View<rtype*> xnodes("xnodes", 1);
+	BasisTools::equidistant_nodes_1D_range(0.0, 10.0, 1, xnodes);
+	Kokkos::View<rtype*> expected("expected", 1);
+	expected(0) = 0.0; 
+
+	for (int i = 0; i < 2; i++){
+		EXPECT_NEAR(expected(i), xnodes(i), DOUBLE_TOL);
+	}
+}
+
 /*
 Tests that two points remain at their locations 
 */
