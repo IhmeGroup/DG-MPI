3aeda00287597ebf9f89183e0f05559fdbda37ed
bbornhoft <bbornhoft@gmail.com>
2022-02-08 16:09:35 -0800

    remove old basis stuff and only have one test for now

--- src/numerics/basis/basis.cpp
+++ src/numerics/basis/basis.cpp
@@ -3,32 +3,9 @@
 namespace Basis {
 
 
-Basis::Basis(BasisType basis_type, const int order){
-
-	this->order = order;
-	get_1d_nodes = BasisTools::equidistant_nodes_1D_range;
-
-	if (basis_type == BasisType::LagrangeEq1D){
-		get_values_pointer = get_values_lagrangeseg;
-		get_grads_pointer = get_grads_lagrangeseg;
-		name = "LagrangeSeg";
-	}
-
-}
-
-void Basis::get_values(Kokkos::View<const rtype**> quad_pts, 
-	Kokkos::View<rtype**> basis_val) {
-
-	get_values_pointer(quad_pts, basis_val, 
-		order, get_1d_nodes);
-}
-
-void Basis::get_grads(Kokkos::View<const rtype**> quad_pts, 
-	Kokkos::View<rtype***> basis_ref_grad) {
-
-	get_grads_pointer(quad_pts, basis_ref_grad, 
-		order, get_1d_nodes);
-}
+/* --------------------------------------
+	LagrangeSeg Method Definitions
+----------------------------------------*/
 void get_values_lagrangeseg(Kokkos::View<const rtype**> quad_pts,
 		Kokkos::View<rtype**> basis_val, const int order, 
 		void (*get_1d_nodes)(rtype, rtype, int,
@@ -68,270 +45,303 @@ void get_grads_lagrangeseg(Kokkos::View<const rtype**> quad_pts,
 }
 
 
-void BasisBase::get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val){
-	throw NotImplementedException("BasisBase does not implement "
-                        "get_values -> implement in child class");
-}
 
-void BasisBase::get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad){
-	throw NotImplementedException("BasisBase does not implement "
-                        "get_grads -> implement in child class");
-}
-
-
-
-
-/* --------------------------------------
-	LagrangeSeg Method Definitions
-----------------------------------------*/
-LagrangeSeg::LagrangeSeg(const int order){
+/* ------------------------------------------------------
+	Basis Method Definitions + Function Pointer Wrappers
+--------------------------------------------------------*/
+Basis::Basis(BasisType basis_type, const int order){
 
 	this->order = order;
-	nb = get_num_basis_coeff(order);
+	get_1d_nodes = BasisTools::equidistant_nodes_1D_range;
 
-	// Set equidistant or GLL nodes (currently only supporting EQ)
-	get_1d_nodes = &BasisTools::equidistant_nodes_1D_range;
-}
-
-void LagrangeSeg::get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val){
-
-	int nq = quad_pts.extent(0);
-
-	if (order == 0){
-		KokkosBlas::fill(basis_val, 1.);
-	}
-	else {
-		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
-		get_1d_nodes(-1., 1., order + 1, xnodes);
-   		for (int iq = 0; iq < nq; iq++){
-        	BasisTools::get_lagrange_basis_val_1D(quad_pts(iq, 0), xnodes, order, 
-        		Kokkos::subview(basis_val, iq, Kokkos::ALL()));
-    	}
+	if (basis_type == BasisType::LagrangeEq1D){
+		get_values_pointer = get_values_lagrangeseg;
+		get_grads_pointer = get_grads_lagrangeseg;
+		name = "LagrangeSeg";
 	}
 
 }
 
-void LagrangeSeg::get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad){
+void Basis::get_values(Kokkos::View<const rtype**> quad_pts, 
+	Kokkos::View<rtype**> basis_val) {
 
-	int nq = quad_pts.extent(0);
-
-	if (order > 0){
-		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
-		get_1d_nodes(-1., 1., order + 1, xnodes);
-		for (int iq = 0; iq < nq; iq++){
-			BasisTools::get_lagrange_basis_grad_1D(quad_pts(iq, 0), xnodes, order,
-				Kokkos::subview(basis_ref_grad, iq, Kokkos::ALL(), 0));
-		}
-	}
+	get_values_pointer(quad_pts, basis_val, 
+		order, get_1d_nodes);
 }
 
+void Basis::get_grads(Kokkos::View<const rtype**> quad_pts, 
+	Kokkos::View<rtype***> basis_ref_grad) {
 
-/* --------------------------------------
-	LagrangeQuad Method Definitions
-----------------------------------------*/
-LagrangeQuad::LagrangeQuad(const int order){
-
-	this->order = order;
-	nb = get_num_basis_coeff(order);
-
-	// Set equidistant or GLL nodes (currently only supporting EQ)
-	get_1d_nodes = &BasisTools::equidistant_nodes_1D_range;
-}
-
-void LagrangeQuad::get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val){
-
-	int nq = quad_pts.extent(0);
-
-	if (order == 0){
-		KokkosBlas::fill(basis_val, 1.);
-	}
-	else {
-		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
-		get_1d_nodes(-1., 1., order + 1, xnodes);
-   		for (int iq = 0; iq < nq; iq++){
-        	BasisTools::get_lagrange_basis_val_2D(quad_pts, xnodes, order, 
-        		basis_val);
-    	}
-	}
-
-}
-
-void LagrangeQuad::get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad){
-
-	int nq = quad_pts.extent(0);
-
-	if (order > 0){
-		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
-		get_1d_nodes(-1., 1., order + 1, xnodes);
-		for (int iq = 0; iq < nq; iq++){
-			BasisTools::get_lagrange_basis_grad_2D(quad_pts, xnodes, order,
-				basis_ref_grad);
-		}
-	}
-}
-
-/* --------------------------------------
-	LagrangeHex Method Definitions
-----------------------------------------*/
-LagrangeHex::LagrangeHex(const int order){
-
-	this->order = order;
-	nb = get_num_basis_coeff(order);
-
-	// Set equidistant or GLL nodes (currently only supporting EQ)
-	get_1d_nodes = &BasisTools::equidistant_nodes_1D_range;
-}
-
-void LagrangeHex::get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val){
-
-	int nq = quad_pts.extent(0);
-
-	if (order == 0){
-		KokkosBlas::fill(basis_val, 1.);
-	}
-	else {
-		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
-		get_1d_nodes(-1., 1., order + 1, xnodes);
-   		for (int iq = 0; iq < nq; iq++){
-        	BasisTools::get_lagrange_basis_val_3D(quad_pts, xnodes, order, 
-        		basis_val);
-    	}
-	}
-
-}
-
-void LagrangeHex::get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad){
-
-	int nq = quad_pts.extent(0);
-
-	if (order > 0){
-		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
-		get_1d_nodes(-1., 1., order + 1, xnodes);
-		for (int iq = 0; iq < nq; iq++){
-			BasisTools::get_lagrange_basis_grad_3D(quad_pts, xnodes, order,
-				basis_ref_grad);
-		}
-	}
-}
-
-
-/* --------------------------------------
-	LegendreSeg Method Definitions
-----------------------------------------*/
-LegendreSeg::LegendreSeg(const int order){
-
-	this->order = order;
-	nb = get_num_basis_coeff(order);
-}
-
-void LegendreSeg::get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val){
-
-	int nq = quad_pts.extent(0);
-
-	if (order == 0){
-		KokkosBlas::fill(basis_val, 1.);
-	}
-	else {
-   		for (int iq = 0; iq < nq; iq++){
-        	BasisTools::get_legendre_basis_val_1D(quad_pts(iq, 0), order, 
-        		Kokkos::subview(basis_val, iq, Kokkos::ALL()));
-    	}
-	}
-
-}
-
-void LegendreSeg::get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad){
-
-	int nq = quad_pts.extent(0);
-
-	if (order > 0){
-		for (int iq = 0; iq < nq; iq++){
-			BasisTools::get_legendre_basis_grad_1D(quad_pts(iq, 0), order,
-				Kokkos::subview(basis_ref_grad, iq, Kokkos::ALL(), 0));
-		}
-	}
+	get_grads_pointer(quad_pts, basis_ref_grad, 
+		order, get_1d_nodes);
 }
 
 
 
-/* --------------------------------------
-	LegendreQuad Method Definitions
-----------------------------------------*/
-LegendreQuad::LegendreQuad(const int order){
+// void BasisBase::get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val){
+// 	throw NotImplementedException("BasisBase does not implement "
+//                         "get_values -> implement in child class");
+// }
 
-	this->order = order;
-	nb = get_num_basis_coeff(order);
-}
-
-void LegendreQuad::get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val){
-
-	int nq = quad_pts.extent(0);
-
-	if (order == 0){
-		KokkosBlas::fill(basis_val, 1.);
-	}
-	else {
-    	BasisTools::get_legendre_basis_val_2D(quad_pts, order, 
-    		basis_val);
-	}
-}
+// void BasisBase::get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad){
+// 	throw NotImplementedException("BasisBase does not implement "
+//                         "get_grads -> implement in child class");
+// }
 
 
-void LegendreQuad::get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad){
-
-	int nq = quad_pts.extent(0);
-
-	if (order > 0){
-		BasisTools::get_legendre_basis_grad_2D(quad_pts, order,
-			basis_ref_grad);	
-	}
-}
-
-/* --------------------------------------
-	LegendreHex Method Definitions
-----------------------------------------*/
-LegendreHex::LegendreHex(const int order){
-
-	this->order = order;
-	nb = get_num_basis_coeff(order);
-}
-
-void LegendreHex::get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val){
-
-	int nq = quad_pts.extent(0);
-
-	if (order == 0){
-		KokkosBlas::fill(basis_val, 1.);
-	}
-	else {
-    	BasisTools::get_legendre_basis_val_3D(quad_pts, order, 
-    		basis_val);
-	}
-}
 
 
-void LegendreHex::get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad){
+// /* --------------------------------------
+// 	LagrangeSeg Method Definitions
+// ----------------------------------------*/
+// LagrangeSeg::LagrangeSeg(const int order){
 
-	int nq = quad_pts.extent(0);
+// 	this->order = order;
+// 	nb = get_num_basis_coeff(order);
 
-	if (order > 0){
-		BasisTools::get_legendre_basis_grad_3D(quad_pts, order,
-			basis_ref_grad);	
-	}
-}
+// 	// Set equidistant or GLL nodes (currently only supporting EQ)
+// 	get_1d_nodes = &BasisTools::equidistant_nodes_1D_range;
+// }
+
+// void LagrangeSeg::get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order == 0){
+// 		KokkosBlas::fill(basis_val, 1.);
+// 	}
+// 	else {
+// 		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
+// 		get_1d_nodes(-1., 1., order + 1, xnodes);
+//    		for (int iq = 0; iq < nq; iq++){
+//         	BasisTools::get_lagrange_basis_val_1D(quad_pts(iq, 0), xnodes, order, 
+//         		Kokkos::subview(basis_val, iq, Kokkos::ALL()));
+//     	}
+// 	}
+
+// }
+
+// void LagrangeSeg::get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order > 0){
+// 		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
+// 		get_1d_nodes(-1., 1., order + 1, xnodes);
+// 		for (int iq = 0; iq < nq; iq++){
+// 			BasisTools::get_lagrange_basis_grad_1D(quad_pts(iq, 0), xnodes, order,
+// 				Kokkos::subview(basis_ref_grad, iq, Kokkos::ALL(), 0));
+// 		}
+// 	}
+// }
+
+
+// /* --------------------------------------
+// 	LagrangeQuad Method Definitions
+// ----------------------------------------*/
+// LagrangeQuad::LagrangeQuad(const int order){
+
+// 	this->order = order;
+// 	nb = get_num_basis_coeff(order);
+
+// 	// Set equidistant or GLL nodes (currently only supporting EQ)
+// 	get_1d_nodes = &BasisTools::equidistant_nodes_1D_range;
+// }
+
+// void LagrangeQuad::get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order == 0){
+// 		KokkosBlas::fill(basis_val, 1.);
+// 	}
+// 	else {
+// 		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
+// 		get_1d_nodes(-1., 1., order + 1, xnodes);
+//    		for (int iq = 0; iq < nq; iq++){
+//         	BasisTools::get_lagrange_basis_val_2D(quad_pts, xnodes, order, 
+//         		basis_val);
+//     	}
+// 	}
+
+// }
+
+// void LagrangeQuad::get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order > 0){
+// 		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
+// 		get_1d_nodes(-1., 1., order + 1, xnodes);
+// 		for (int iq = 0; iq < nq; iq++){
+// 			BasisTools::get_lagrange_basis_grad_2D(quad_pts, xnodes, order,
+// 				basis_ref_grad);
+// 		}
+// 	}
+// }
+
+// /* --------------------------------------
+// 	LagrangeHex Method Definitions
+// ----------------------------------------*/
+// LagrangeHex::LagrangeHex(const int order){
+
+// 	this->order = order;
+// 	nb = get_num_basis_coeff(order);
+
+// 	// Set equidistant or GLL nodes (currently only supporting EQ)
+// 	get_1d_nodes = &BasisTools::equidistant_nodes_1D_range;
+// }
+
+// void LagrangeHex::get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order == 0){
+// 		KokkosBlas::fill(basis_val, 1.);
+// 	}
+// 	else {
+// 		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
+// 		get_1d_nodes(-1., 1., order + 1, xnodes);
+//    		for (int iq = 0; iq < nq; iq++){
+//         	BasisTools::get_lagrange_basis_val_3D(quad_pts, xnodes, order, 
+//         		basis_val);
+//     	}
+// 	}
+
+// }
+
+// void LagrangeHex::get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order > 0){
+// 		Kokkos::View<rtype*> xnodes("xnodes", order + 1);
+// 		get_1d_nodes(-1., 1., order + 1, xnodes);
+// 		for (int iq = 0; iq < nq; iq++){
+// 			BasisTools::get_lagrange_basis_grad_3D(quad_pts, xnodes, order,
+// 				basis_ref_grad);
+// 		}
+// 	}
+// }
+
+
+// /* --------------------------------------
+// 	LegendreSeg Method Definitions
+// ----------------------------------------*/
+// LegendreSeg::LegendreSeg(const int order){
+
+// 	this->order = order;
+// 	nb = get_num_basis_coeff(order);
+// }
+
+// void LegendreSeg::get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order == 0){
+// 		KokkosBlas::fill(basis_val, 1.);
+// 	}
+// 	else {
+//    		for (int iq = 0; iq < nq; iq++){
+//         	BasisTools::get_legendre_basis_val_1D(quad_pts(iq, 0), order, 
+//         		Kokkos::subview(basis_val, iq, Kokkos::ALL()));
+//     	}
+// 	}
+
+// }
+
+// void LegendreSeg::get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order > 0){
+// 		for (int iq = 0; iq < nq; iq++){
+// 			BasisTools::get_legendre_basis_grad_1D(quad_pts(iq, 0), order,
+// 				Kokkos::subview(basis_ref_grad, iq, Kokkos::ALL(), 0));
+// 		}
+// 	}
+// }
+
+
+
+// /* --------------------------------------
+// 	LegendreQuad Method Definitions
+// ----------------------------------------*/
+// LegendreQuad::LegendreQuad(const int order){
+
+// 	this->order = order;
+// 	nb = get_num_basis_coeff(order);
+// }
+
+// void LegendreQuad::get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order == 0){
+// 		KokkosBlas::fill(basis_val, 1.);
+// 	}
+// 	else {
+//     	BasisTools::get_legendre_basis_val_2D(quad_pts, order, 
+//     		basis_val);
+// 	}
+// }
+
+
+// void LegendreQuad::get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order > 0){
+// 		BasisTools::get_legendre_basis_grad_2D(quad_pts, order,
+// 			basis_ref_grad);	
+// 	}
+// }
+
+// /* --------------------------------------
+// 	LegendreHex Method Definitions
+// ----------------------------------------*/
+// LegendreHex::LegendreHex(const int order){
+
+// 	this->order = order;
+// 	nb = get_num_basis_coeff(order);
+// }
+
+// void LegendreHex::get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order == 0){
+// 		KokkosBlas::fill(basis_val, 1.);
+// 	}
+// 	else {
+//     	BasisTools::get_legendre_basis_val_3D(quad_pts, order, 
+//     		basis_val);
+// 	}
+// }
+
+
+// void LegendreHex::get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad){
+
+// 	int nq = quad_pts.extent(0);
+
+// 	if (order > 0){
+// 		BasisTools::get_legendre_basis_grad_3D(quad_pts, order,
+// 			basis_ref_grad);	
+// 	}
+// }
 
 } // end namespace Basis
--- src/numerics/basis/basis.h
+++ src/numerics/basis/basis.h
@@ -76,191 +76,191 @@ Child classes (available basis functions) of this base class include:
 	  triangles, hexahedrons, and (eventually) tetrahedrons)]
 	- Legendre basis [support for segments and quadrilaterals]
 */
-class BasisBase {
-public:
-	/*
-	Virtual destructor
-	*/
-	virtual ~BasisBase() = default;
+// class BasisBase {
+// public:
+// 	/*
+// 	Virtual destructor
+// 	*/
+// 	virtual ~BasisBase() = default;
 
 	
-	inline int get_order(){return order;}
-	/*
-	This is a function pointer that get assigned a function from
-	the BasisTools namespace that either defines equidistant
-	nodes or gauss-lobatto nodes. 
+// 	inline int get_order(){return order;}
+// 	/*
+// 	This is a function pointer that get assigned a function from
+// 	the BasisTools namespace that either defines equidistant
+// 	nodes or gauss-lobatto nodes. 
 
-	NOTE: Currently only supporting
-	equidistant nodes. This function is set in the constructor
-	of the Lagrange classes for now but needs to be moved to a 
-	setter function later once enums are figured out.
+// 	NOTE: Currently only supporting
+// 	equidistant nodes. This function is set in the constructor
+// 	of the Lagrange classes for now but needs to be moved to a 
+// 	setter function later once enums are figured out.
 
-	This function gets the 1D coordinates in reference space
+// 	This function gets the 1D coordinates in reference space
 
-	Inputs:
-	-------
-		start: start of ref space (typically -1)
-		stop:  end of ref space (typically 1)
-		nnodes: num of nodes in 1D ref space
+// 	Inputs:
+// 	-------
+// 		start: start of ref space (typically -1)
+// 		stop:  end of ref space (typically 1)
+// 		nnodes: num of nodes in 1D ref space
 
-	Outputs:
-	--------
-		xnodes: coordinates of nodes in 1D ref space [nnodes]
-	*/
-	void (*get_1d_nodes)(rtype start, rtype stop, int nnodes,
-		Kokkos::View<rtype*> &xnodes);
+// 	Outputs:
+// 	--------
+// 		xnodes: coordinates of nodes in 1D ref space [nnodes]
+// 	*/
+// 	void (*get_1d_nodes)(rtype start, rtype stop, int nnodes,
+// 		Kokkos::View<rtype*> &xnodes);
 
-	/*
-	Calculates the basis values
+// 	/*
+// 	Calculates the basis values
 
-	Inputs:
-	-------
-		quad_pts: coordinates of quadrature points [nb, ndims]
+// 	Inputs:
+// 	-------
+// 		quad_pts: coordinates of quadrature points [nb, ndims]
 
-	Outputs:
-	--------
-		basis_val: evaluated basis function [nq, nb]
-	*/
-	virtual void get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val);
+// 	Outputs:
+// 	--------
+// 		basis_val: evaluated basis function [nq, nb]
+// 	*/
+// 	virtual void get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val);
 
-	/*
-	Calculates basis gradient (in reference space)
+// 	/*
+// 	Calculates basis gradient (in reference space)
 
-	Inputs:
-	-------
-		quad_pts: coordinates of quadrature points [nb, ndims]
+// 	Inputs:
+// 	-------
+// 		quad_pts: coordinates of quadrature points [nb, ndims]
 
-	Outputs:
-	--------
-		basis_ref_grad: evaluated gradient of basis function in
-			reference space [nq, nb, ndims]
-	*/
-	virtual void get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad);
+// 	Outputs:
+// 	--------
+// 		basis_ref_grad: evaluated gradient of basis function in
+// 			reference space [nq, nb, ndims]
+// 	*/
+// 	virtual void get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad);
 
-protected:
-	int nb; //number of polynomial coefficients
-	int order; // polynomial or geometric order
-};
-
-
-class LagrangeSeg : public BasisBase, public SegShape {
-public:
-	/*
-	Class constructor
-	*/
-	LagrangeSeg(const int order);
-
-	/*
-	Class destructor
-	*/
-	~LagrangeSeg() = default;
-
-	void get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val) override;
-
-	void get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad) override;
-};
-
-class LagrangeQuad : public BasisBase, public QuadShape {
-public:
-	/*
-	Class constructor
-	*/
-	LagrangeQuad(const int order);
-
-	/*
-	Class destructor
-	*/
-	~LagrangeQuad() = default;
-
-	void get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val) override;
-
-	void get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad) override;
-};
-
-class LagrangeHex : public BasisBase, public HexShape {
-public:
-	/*
-	Class constructor
-	*/
-	LagrangeHex(const int order);
-
-	/*
-	Class destructor
-	*/
-	~LagrangeHex() = default;
-
-	void get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val) override;
-
-	void get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad) override;
-};
+// protected:
+// 	int nb; //number of polynomial coefficients
+// 	int order; // polynomial or geometric order
+// };
 
 
-class LegendreSeg : public BasisBase, public SegShape {
-public:
-	/*
-	Class constructor
-	*/
-	LegendreSeg(const int order);
+// class LagrangeSeg : public BasisBase, public SegShape {
+// public:
+// 	/*
+// 	Class constructor
+// 	*/
+// 	LagrangeSeg(const int order);
 
-	/*
-	Class destructor
-	*/
-	~LegendreSeg() = default;
+// 	/*
+// 	Class destructor
+// 	*/
+// 	~LagrangeSeg() = default;
 
-	void get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val) override;
+// 	void get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val) override;
 
-	void get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad) override;
-};
+// 	void get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad) override;
+// };
 
-class LegendreQuad : public BasisBase, public QuadShape {
-public:
-	/*
-	Class constructor
-	*/
-	LegendreQuad(const int order);
+// class LagrangeQuad : public BasisBase, public QuadShape {
+// public:
+// 	/*
+// 	Class constructor
+// 	*/
+// 	LagrangeQuad(const int order);
 
-	/*
-	Class destructor
-	*/
-	~LegendreQuad() = default;
+// 	/*
+// 	Class destructor
+// 	*/
+// 	~LagrangeQuad() = default;
 
-	void get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val) override;
+// 	void get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val) override;
 
-	void get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad) override;
+// 	void get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad) override;
+// };
 
-};
+// class LagrangeHex : public BasisBase, public HexShape {
+// public:
+// 	/*
+// 	Class constructor
+// 	*/
+// 	LagrangeHex(const int order);
 
-class LegendreHex : public BasisBase, public HexShape {
-public:
-	/*
-	Class constructor
-	*/
-	LegendreHex(const int order);
+// 	/*
+// 	Class destructor
+// 	*/
+// 	~LagrangeHex() = default;
 
-	/*
-	Class destructor
-	*/
-	~LegendreHex() = default;
+// 	void get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val) override;
 
-	void get_values(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype**> basis_val) override;
+// 	void get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad) override;
+// };
 
-	void get_grads(Kokkos::View<const rtype**> quad_pts,
-		Kokkos::View<rtype***> basis_ref_grad) override;
 
-};
+// class LegendreSeg : public BasisBase, public SegShape {
+// public:
+// 	/*
+// 	Class constructor
+// 	*/
+// 	LegendreSeg(const int order);
+
+// 	/*
+// 	Class destructor
+// 	*/
+// 	~LegendreSeg() = default;
+
+// 	void get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val) override;
+
+// 	void get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad) override;
+// };
+
+// class LegendreQuad : public BasisBase, public QuadShape {
+// public:
+// 	/*
+// 	Class constructor
+// 	*/
+// 	LegendreQuad(const int order);
+
+// 	/*
+// 	Class destructor
+// 	*/
+// 	~LegendreQuad() = default;
+
+// 	void get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val) override;
+
+// 	void get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad) override;
+
+// };
+
+// class LegendreHex : public BasisBase, public HexShape {
+// public:
+// 	/*
+// 	Class constructor
+// 	*/
+// 	LegendreHex(const int order);
+
+// 	/*
+// 	Class destructor
+// 	*/
+// 	~LegendreHex() = default;
+
+// 	void get_values(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype**> basis_val) override;
+
+// 	void get_grads(Kokkos::View<const rtype**> quad_pts,
+// 		Kokkos::View<rtype***> basis_ref_grad) override;
+
+// };
 
 
 // class BasisFactory {
