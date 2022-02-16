25ae97dce82663bd5fc8993d98b13cdd7a213aa6
alilasemi <alasemi@stanford.edu>
2022-02-15 19:02:33 -0800

    Remove using statement from .h file

--- src/common/enums.h
+++ src/common/enums.h
@@ -8,22 +8,19 @@
 
 #include "common/my_exceptions.h"
 
-using std::cout, std::endl;
-using std::string, std::vector;
-
 // This template is used to point towards the correct array containing the enum
 // string names.
 template <class T>
-vector<string> get_enum_to_string_array();
+std::vector<std::string> get_enum_to_string_array();
 
 // This template returns the string-valued name of an enum value. The template
 // parameter should be set to the enum type.
 // Example:
 //     enum_to_string<BasisType>("Legendre2D")
 template <class T>
-string enum_to_string(T enum_value) {
+std::string enum_to_string(T enum_value) {
     // Get array of enum to string
-    vector<string> enum_to_string = get_enum_to_string_array<T>();
+    std::vector<std::string> enum_to_string = get_enum_to_string_array<T>();
     // Return the string
     return enum_to_string[enum_value];
 }
@@ -33,9 +30,9 @@ string enum_to_string(T enum_value) {
 // Example:
 //     enum_from_string<BasisType>(BasisType::Legendre2D)
 template <class T>
-T enum_from_string(string str) {
+T enum_from_string(std::string str) {
     // Get array of enum to string
-    vector<string> enum_to_string = get_enum_to_string_array<T>();
+    std::vector<std::string> enum_to_string = get_enum_to_string_array<T>();
     // Find index of enum
     auto index_it = std::find(std::begin(enum_to_string),
             std::end(enum_to_string), str);
@@ -44,10 +41,10 @@ T enum_from_string(string str) {
         // If it doesn't exist, then print the available choices and throw an
         // exception
         std::stringstream msg;
-        msg << endl << endl << "Oh no! " << str << " is not a valid choice!" << endl;
-        msg << "Instead, try one of these:" << endl;
+        msg << std::endl << std::endl << "Oh no! " << str << " is not a valid choice!" << std::endl;
+        msg << "Instead, try one of these:" << std::endl;
         for (auto it = std::begin(enum_to_string); it != std::end(enum_to_string); it++) {
-            msg << "    " << *it << endl;
+            msg << "    " << *it << std::endl;
         }
         throw InputException(msg.str());
     }
@@ -71,8 +68,8 @@ enum BasisType {
     TriangleLagrange
 };
 template <>
-inline vector<string> get_enum_to_string_array<BasisType> () {
-    return vector<string> {
+inline std::vector<std::string> get_enum_to_string_array<BasisType> () {
+    return std::vector<std::string> {
         "LagrangeSeg",
         "LagrangeQuad",
         "LagrangeHex",
@@ -95,8 +92,8 @@ enum QuadratureType {
     TriangleGaussLegendre
 };
 template <>
-inline vector<string> get_enum_to_string_array<QuadratureType> () {
-    return vector<string> {
+inline std::vector<std::string> get_enum_to_string_array<QuadratureType> () {
+    return std::vector<std::string> {
         "GaussLegendre",
         "GaussLobatto",
         "TriangleDunavant",
@@ -109,8 +106,8 @@ enum NodeType {
     Interior // equidistant nodes but in the interior of the reference element, only useful for uniform quad/hex meshes
 };
 template <>
-inline vector<string> get_enum_to_string_array<NodeType> () {
-    return vector<string> {
+inline std::vector<std::string> get_enum_to_string_array<NodeType> () {
+    return std::vector<std::string> {
         "Equidistant",
         "Interior"
     };
@@ -122,8 +119,8 @@ enum NormType {
     L2
 };
 template <>
-inline vector<string> get_enum_to_string_array<NormType> () {
-    return vector<string> {
+inline std::vector<std::string> get_enum_to_string_array<NormType> () {
+    return std::vector<std::string> {
         "DomainIntegral",
         "L1",
         "L2"
@@ -137,8 +134,8 @@ enum ShapeType {
     Triangle
 };
 template <>
-inline vector<string> get_enum_to_string_array<ShapeType> () {
-    return vector<string> {
+inline std::vector<std::string> get_enum_to_string_array<ShapeType> () {
+    return std::vector<std::string> {
         "Segment",
         "Quadrilateral",
         "Hexahedron",
@@ -155,8 +152,8 @@ enum DGSchemeType {
     None
 };
 template <>
-inline vector<string> get_enum_to_string_array<DGSchemeType> () {
-    return vector<string> {
+inline std::vector<std::string> get_enum_to_string_array<DGSchemeType> () {
+    return std::vector<std::string> {
         "DG",
         "SumFact",
         "LegendreStruct",
@@ -171,8 +168,8 @@ enum InitType {
     Interpolation
 };
 template <>
-inline vector<string> get_enum_to_string_array<InitType> () {
-    return vector<string> {
+inline std::vector<std::string> get_enum_to_string_array<InitType> () {
+    return std::vector<std::string> {
         "L2Projection",
         "Interpolation"
     };
