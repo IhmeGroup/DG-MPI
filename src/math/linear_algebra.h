18db3723f249d00603f6310c94614e35bbcf0ccf
bbornhoft <bbornhoft@gmail.com>
2022-02-17 19:56:09 -0800

    add identity, gemms, and determinant of a 2x2 or 3x3

--- src/math/linear_algebra.h
+++ src/math/linear_algebra.h
@@ -8,15 +8,32 @@
 #include "KokkosBatched_LU_Serial_Impl.hpp"
 #include "KokkosBatched_LU_Team_Impl.hpp"
 #include "KokkosBatched_SolveLU_Decl.hpp"
-// #include<KokkosBlas_gesv.hpp>
+
+#include "KokkosBatched_Gemm_Decl.hpp"
+#include "KokkosBatched_Gemm_Serial_Impl.hpp"
+#include "KokkosBatched_Gemm_Team_Impl.hpp"
+
 
 using namespace KokkosBatched;
 
 namespace Math {
 
 	template<typename ViewType> KOKKOS_INLINE_FUNCTION
-	void invA(const ViewType& mat, ViewType& imat);
+	void identity(ViewType mat);
 
+	template<typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
+	void invA(const ViewType1 mat, ViewType2 imat);
+
+	template<typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
+	void cAxBT_to_C(rtype c, const ViewType1& A, 
+		const ViewType2& B, ViewType2& C);
+	
+	template<typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
+	void cATxB_to_C(rtype c, const ViewType1& A, 
+		const ViewType2& B, ViewType2& C);
+	
+	template<typename ViewType> KOKKOS_INLINE_FUNCTION
+	void det(const ViewType &mat, rtype &det);
 }
 
 #include "math/linear_algebra.cpp"
