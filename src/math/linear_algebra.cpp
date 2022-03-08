0395f249252f7878cf8aabbb1e2aa80f396282a8
bbornhoft <bbornhoft@gmail.com>
2022-03-08 11:23:50 -0800

    Clean up MATH namespace. This includes:
    - removing specific team_ functions and using fcn overloading
    - adding comments throughout and some TODOs
    - addition of a few new functions such as cAxB_to_C, etc...

--- src/math/linear_algebra.cpp
+++ src/math/linear_algebra.cpp
@@ -3,12 +3,14 @@ namespace Math {
 
     template<typename ViewType> KOKKOS_INLINE_FUNCTION
     void identity(ViewType mat){
-        for (unsigned i = 0; i < mat.extent(0); i++){
-            for (unsigned j = 0; j < mat.extent(1); j++){
+        // TODO: this should be parrallelized
+        for (unsigned long i = 0; i < mat.extent(0); i++){
+            for (unsigned long j = 0; j < mat.extent(1); j++){
                 if ( i == j ) {mat(i, j) = 1.;}
             }
         }
     }
+
 
     template<typename ScalarType, typename ViewType> KOKKOS_INLINE_FUNCTION
     void cA_to_A(const ScalarType c, ViewType A){
@@ -17,52 +19,61 @@ namespace Math {
 
 
     template<typename MemberType, typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
-    void team_copy_A_to_B(const ViewType1& A, ViewType2& B, const MemberType& member){
+    void copy_A_to_B(const ViewType1& A, ViewType2& B, const MemberType& member){
         TeamCopy<MemberType, Trans::NoTranspose>::invoke(member, A, B);
     }
 
 
     template<typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
     void invA(const ViewType1 mat, ViewType2 imat){
-
+        // make sure that the matrices are square
         assert(mat.extent(0) == mat.extent(1));
         assert(imat.extent(0) == imat.extent(1));
         // need to make imat identity prior to solve
         identity(imat);
+        // get the LU decomposition
         SerialLU<Algo::Level3::Unblocked>::invoke(mat);
+        // solve the LU=I to make I=(UL)^-1
         SerialSolveLU<Trans::NoTranspose, Algo::Level3::Unblocked>
             ::invoke(mat, imat);
     }
 
-    template<typename MemberType, typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
-    void team_invA(const ViewType1 mat, ViewType2 imat, const MemberType& member){
 
+    template<typename MemberType, typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
+    void invA(const ViewType1 mat, ViewType2 imat, const MemberType& member){
+        // make sure that the matrices are square
         assert(mat.extent(0) == mat.extent(1));
         assert(imat.extent(0) == imat.extent(1));
         // need to make imat identity prior to solve
         identity(imat);
+        // get the LU decomposition
         TeamLU<MemberType, Algo::Level3::Unblocked>::invoke(member, mat);
+        // solve the LU=I to make I=(UL)^-1
         TeamSolveLU<MemberType, Trans::NoTranspose, Algo::Level3::Unblocked>
             ::invoke(member, mat, imat);
     }
 
     template<typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
     void cAxBT_to_C(rtype c, const ViewType1& A, const ViewType2& B, ViewType2& C){
-
+        
         SerialGemm<Trans::NoTranspose, Trans::Transpose, Algo::Gemm::Unblocked>
             ::invoke(c, A, B, 0., C);
     }
 
+
     template<typename ViewType1, typename ViewType2, typename ViewType3> KOKKOS_INLINE_FUNCTION
     void cATxB_to_C(rtype c, const ViewType1& A, const ViewType2& B, ViewType3& C){
-
+        
         SerialGemm<Trans::Transpose, Trans::NoTranspose, Algo::Gemm::Unblocked>
             ::invoke(c, A, B, 0., C);
     }
 
-    template<typename MemberType, typename ViewType1, typename ViewType2, typename ViewType3> KOKKOS_INLINE_FUNCTION
-    void team_cATxB_to_C(rtype c, const ViewType1& A, const ViewType2& B, ViewType3& C, const MemberType& member){
 
+    template<typename MemberType, typename ViewType1, typename ViewType2, 
+    typename ViewType3> KOKKOS_INLINE_FUNCTION
+    void cATxB_to_C(rtype c, const ViewType1& A, const ViewType2& B, ViewType3& C, 
+        const MemberType& member){
+        
         TeamGemm<MemberType, Trans::Transpose, Trans::NoTranspose, Algo::Gemm::Unblocked>
             ::invoke(member, c, A, B, 0., C);
     }
@@ -72,6 +83,13 @@ namespace Math {
 
         SerialGemm<Trans::NoTranspose, Trans::NoTranspose, Algo::Gemm::Unblocked>
             ::invoke(c, A, B, 0., C);
+    }
+
+    template<typename MemberType, typename ViewType1, typename ViewType2, typename ViewType3> KOKKOS_INLINE_FUNCTION
+    void cAxB_to_C(rtype c, const ViewType1& A, const ViewType2& B, ViewType3& C, const MemberType& member){
+
+        TeamGemm<MemberType, Trans::NoTranspose, Trans::NoTranspose, Algo::Gemm::Unblocked>
+            ::invoke(member, c, A, B, 0., C);
     }
 
     template<typename ViewType> KOKKOS_INLINE_FUNCTION
--- src/math/linear_algebra.h
+++ src/math/linear_algebra.h
@@ -23,41 +23,168 @@
 
 using namespace KokkosBatched;
 
-namespace Math {
+/*
+namespace Math
+--------------
 
+The following namespace provides wrappers around various math operations. 
+Each function utilizes Kokkos kernels to conduct various mathematical 
+operations. (Example: computing the inverse of matrix A)
+
+Some things to note:
+
+    1. We use function overloading to access the serial vs team versions 
+       of the Kokkos batched functions. For the team versions, we must pass
+       the member of the TeamPolicy.
+    2. These are currently only batched Kokkos kernels wrappers, meaning
+       each time they are called they MUST be called inside a Kokkos 
+       parallel_for
+
+    TODO: 
+       Add overloaded kernels for large gemms
+       Add additional math tests
+*/
+namespace Math {
+    /*
+    Construct an identity matrix
+
+    Outputs:
+    --------
+        mat: identity matrix sized by the extent of the provided View
+    */
     template<typename ViewType> KOKKOS_INLINE_FUNCTION
     void identity(ViewType mat);
 
+    /*
+    Scales a matrix A by constant c
+
+    Inputs:
+    -------
+        c: scalar value
+        A: matrix to be scaled
+    Outputs:
+    --------
+        A: returned A matrix is scaled by c
+    */
     template<typename ScalarType, typename ViewType> KOKKOS_INLINE_FUNCTION
     void cA_to_A(const ScalarType c, ViewType A);
+    
 
+    /* 
+    TODO: Implement serial copy of A to B
+    */
+
+    /*
+    Team copy of A to B
+    This function takes the View A and directly copies it into View B
+    */
     template<typename MemberType, typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
-    void team_copy_A_to_B(const ViewType1& A, ViewType2& B, const MemberType& member);
+    void copy_A_to_B(const ViewType1& A, ViewType2& B, const MemberType& member);
 
+    /*
+    Computes the inverse of the matrix A
+
+    Inputs:
+    -------
+        mat: matrix to be inverted
+    Outputs:
+    --------
+        imat: inverted matrix
+    */
     template<typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
     void invA(const ViewType1 mat, ViewType2 imat);
 
+    /*
+    Team version of computing the inverse of A
+    */
     template<typename MemberType, typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
-    void team_invA(const ViewType1 mat, ViewType2 imat, const MemberType& member);
+    void invA(const ViewType1 mat, ViewType2 imat, const MemberType& member);
 
+    /*
+    Computes cAB^T and stores in C
+
+    Inputs:
+    -------
+        c: scalar value
+        A: matrix shape [n x m]
+        B: matrix shape [p x m] (note the transpose is taken)
+
+    Outputs:
+    --------
+        C: matrix shape [n x p]
+    */
     template<typename ViewType1, typename ViewType2> KOKKOS_INLINE_FUNCTION
     void cAxBT_to_C(rtype c, const ViewType1& A,
         const ViewType2& B, ViewType2& C);
 
+    /*
+    TODO: Add team version of cAxBT_to_C
+    */
+
+    /*
+    Computes cA^T*B and stores in C
+
+    Inputs:
+    -------
+        c: scalar value
+        A: matrix shape [n x m]
+        B: matrix shape [n x p]
+
+    Outputs:
+    --------
+        C: matrix shape [m x p]
+    */
     template<typename ViewType1, typename ViewType2, 
     typename ViewType3> KOKKOS_INLINE_FUNCTION
     void cATxB_to_C(rtype c, const ViewType1& A,
         const ViewType2& B, ViewType3& C);
 
+    /*
+    Team version of cA^T*B -> C
+    */
     template<typename MemberType, typename ViewType1, typename ViewType2, 
     typename ViewType3> KOKKOS_INLINE_FUNCTION
-    void team_cATxB_to_C(rtype c, const ViewType1& A, const ViewType2& B, 
+    void cATxB_to_C(rtype c, const ViewType1& A, const ViewType2& B, 
         ViewType3& C, const MemberType& member);
 
+    /*
+    Computes cAB and stores in C
+
+    Inputs:
+    -------
+        c: scalar
+        A: matrix shape [n x m]
+        B: matrix shape [m x p]
+
+    Outputs:
+    --------
+        C: matrix shape [n x p]
+    */
     template<typename ViewType1, typename ViewType2, typename ViewType3> KOKKOS_INLINE_FUNCTION
     void cAxB_to_C(rtype c, const ViewType1& A,
         const ViewType2& B, ViewType3& C);
 
+    /*
+    Team version of cAB -> C
+    */
+    template<typename MemberType, typename ViewType1, typename ViewType2, 
+    typename ViewType3> KOKKOS_INLINE_FUNCTION
+    void cAxB_to_C(rtype c, const ViewType1& A, const ViewType2& B, ViewType3& C, 
+        const MemberType& member);
+
+    /*
+    Calculates the determinant of a matrix
+
+    Note: Not general, only works with 1x1, 2x2, and 3x3 matrices
+
+    Inputs:
+    -------
+        mat: matrix shape [n x n]
+
+    Outputs:
+    --------
+        det: determinant of mat
+    */
     template<typename ViewType> KOKKOS_INLINE_FUNCTION
     void det(const ViewType &mat, rtype &det);
 }
