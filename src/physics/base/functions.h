442faa28d7d3ece2e529e2069200daf9f2f77146
bbornhoft <bbornhoft@gmail.com>
2022-04-11 16:49:46 -0700

    prepare infrastructure for flux functions

--- src/physics/base/functions.h
+++ src/physics/base/functions.h
@@ -1,78 +1,14 @@
-// #ifndef DG_PHYSICS_BASE_FUNCTIONS_H
-// #define DG_PHYSICS_BASE_FUNCTIONS_H
+#ifndef DG_PHYSICS_BASE_FUNCTIONS_H
+#define DG_PHYSICS_BASE_FUNCTIONS_H
 
-// #include "common/defines.h"
+#include "common/defines.h"
+#include "common/enums.h"
 // #include "physics/base/data.h"
 
-// #include <Kokkos_Core.hpp>
-// #include "KokkosBlas1_nrm2.hpp"
-// #include "KokkosBlas1_axpby.hpp"
-// #include "KokkosBlas1_scal.hpp"
-
-
-// namespace BaseFcnType {
-
-// /*
-// Enum class that stores the types of analytical functions for initial
-// conditions, exact solutions, and/or boundary conditions. These
-// functions are generalizable to different kinds of physics.
-// */
-// enum class FcnType {
-//     Uniform,
-//     None,
-// };
-
-// /*
-// ---------------
-// State functions
-// ---------------
-// These classes inherit from the FcnBase class. See FcnBase for detailed
-// comments of attributes and methods. Information specific to the
-// corresponding child classes can be found below. These classes should
-// correspond to the FcnType enum members above.
-// */
-
-// /*
-// This class sets a uniform state.\
-// */
-// class Uniform: public FcnBase {
-//     public:
-//         /*
-//         Constructor sets read in state
-
-//         Inputs:
-//         -------
-//             physics: physics object
-//         Outpus:
-//         -------
-//             state: input condition for uniform condition [ns]
-//         */
-//         template<int dim>
-//         Uniform(Physics::PhysicsBase<dim> &physics, const rtype *state);
-
-//         /*
-//         Destructor
-//         */
-//         ~Uniform();
-
-        
-//         Set Uniform state as given x-coordinates and time
-
-//         This function takes the stored state from the physics class and sets the values
-//         in the solvers state
-
-//         Inputs:
-//         -------
-//             physics: physics object
-//             x: coordinates in physical space [ndims]
-//             t: time
-        
-//         template<int dim> DG_KOKKOS_FUNCTION
-//         void get_state(Physics::PhysicsBase<dim> &physics, const rtype *x, const rtype *t,
-//             Kokkos::View<rtype*> Uq);
-// };
-
-// } // end namespace BaseFcnType
+#include <Kokkos_Core.hpp>
+#include "KokkosBlas1_nrm2.hpp"
+#include "KokkosBlas1_axpby.hpp"
+#include "KokkosBlas1_scal.hpp"
 
 
 // /*
@@ -85,7 +21,7 @@
 // be found below. These classes should correspond to the ConvNumFluxType
 // or DiffNumFluxType enum members.
 // */
-// namespace BaseConvNumFluxType {
+namespace BaseConvNumFluxType {
 
 // /*
 // Enum class that stores the types of convective numerical fluxes. These
@@ -109,6 +45,8 @@
 //             Kokkos::View<rtype*> Fq);
 // };
 
-// } // end namespace BaseConvNumFluxType
+} // end namespace BaseConvNumFluxType
 
-// #endif //DG_PHYSICS_BASE_FUNCTIONS_H
+
+
+#endif //DG_PHYSICS_BASE_FUNCTIONS_H
