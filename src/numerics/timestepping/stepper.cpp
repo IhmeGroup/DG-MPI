dd2b0a3fca526826e8d65044b7f42c385af6e796
bbornhoft <bbornhoft@gmail.com>
2022-04-20 14:17:40 -0700

    add take_time_step functions for both FE and RK4

--- src/numerics/timestepping/stepper.cpp
+++ src/numerics/timestepping/stepper.cpp
@@ -1,5 +1,6 @@
 #include "numerics/timestepping/stepper.h"
 #include "solver/base.h"
+#include "solver/tools.h"
 
 template<unsigned dim>
 void StepperBase<dim>::take_time_step(Solver<dim>& solver){
@@ -8,18 +9,141 @@ void StepperBase<dim>::take_time_step(Solver<dim>& solver){
 
 template<unsigned dim>
 void FE<dim>::take_time_step(Solver<dim>& solver){
-
-
+    // auto dt = this->get_time_step();
+    auto iMM_elems = solver.vol_helpers.iMM_elems;
     solver.get_residual();
 
-    // TODO: Multiply by inverse mass matrix
-    // TODO: Update solution state
+    // Multiply by inverse mass matrix
+    view_type_3D dU("dU", solver.res.extent(0), 
+        solver.res.extent(1), solver.res.extent(2));
+    SolverTools::mult_inv_mass_matrix(dt, iMM_elems, solver.res, dU);
+   
+    // solver.network.print_3d_view(iMM_elems);
+    // solver.network.print_3d_view(dU);
+    // printf("dt = %f\n", dt);
+    // Copy back to host (TODO: Remove after debugging)
+    // auto h_res = Kokkos::create_mirror_view_and_copy(
+    //         Kokkos::DefaultHostExecutionSpace{}, solver.res);
+    // auto h_dU = Kokkos::create_mirror_view_and_copy(
+    //         Kokkos::DefaultHostExecutionSpace{}, dU);
+    // solver.network.print_3d_view(h_res);
+    // solver.network.print_3d_view(h_dU);
 
+    // //Update solution state
+    // printf("BEFORE SOLUTION UPDATE\n");
+    // Kokkos::deep_copy(solver.h_Uc, solver.Uc);
+    // solver.network.print_3d_view(solver.h_Uc);
+
+    const unsigned num_entries = dU.extent(0) * dU.extent(1) * dU.extent(2);
+    Math::cApB_to_B(num_entries, 1., dU.data(), solver.Uc.data());
+    solver.time += dt;
+
+    // solver.network.barrier();
+    // printf("AFTER SOLUTION UPDATE\n");
+    // Kokkos::deep_copy(solver.h_Uc, solver.Uc);
+    // solver.network.print_3d_view(solver.h_Uc);
     // TODO: Add limiter call here? Or somewhere else?
 
+}
+
+template<unsigned dim>
+void RK4<dim>::take_time_step(Solver<dim>& solver){
+
+    // unpack
+    auto iMM_elems = solver.vol_helpers.iMM_elems;
+    unsigned num_entries;
+
+    // TODO: Figure out a better way to do this? Maybe just implement
+    // the low storage version of RK4...
+    // allocate stage memory (This is likely not memory efficient...)
+    view_type_3D dU1("dU1", solver.res.extent(0), 
+        solver.res.extent(1), solver.res.extent(2));
+
+    view_type_3D dU2("dU2", solver.res.extent(0), 
+        solver.res.extent(1), solver.res.extent(2));
+    
+    view_type_3D dU3("dU3", solver.res.extent(0),
+        solver.res.extent(1), solver.res.extent(2));
+    
+    view_type_3D dU4("dU4", solver.res.extent(0), 
+        solver.res.extent(1), solver.res.extent(2));
+
+    view_type_3D Uhold("Uhold", solver.res.extent(0),
+        solver.res.extent(1), solver.res.extent(2));
+
+    Kokkos::deep_copy(Uhold, solver.Uc); // put the state coeffs in Utemp
+
+    /* ---------------------------------------------- */
+    /*                  First stage                   */
+    /* ---------------------------------------------- */
+    solver.get_residual();
+    // Multiply by inverse mass matrix
+    SolverTools::mult_inv_mass_matrix(dt, iMM_elems, solver.res, dU1);
+    
+    num_entries = dU1.extent(0) * dU1.extent(1) * dU1.extent(2);
+    Math::cApB_to_C(num_entries, 0.5, dU1.data(), Uhold.data(), solver.Uc.data());
+    // TODO: Add limiter call here? Or somewhere else?
+
+    // printf("--------- STAGE 1 ---------\n");
+    // Kokkos::deep_copy(solver.h_Uc, solver.Uc);
+    // solver.network.print_3d_view(solver.h_Uc);
+    // solver.network.print_3d_view(Uhold);
+
+    /* ---------------------------------------------- */
+    /*                 Second stage                   */
+    /* ---------------------------------------------- */
+    solver.time += dt / 2.0;
+    solver.get_residual();
+    SolverTools::mult_inv_mass_matrix(dt, iMM_elems, solver.res, dU2);
+
+    num_entries = dU2.extent(0) * dU2.extent(1) * dU2.extent(2);
+    Math::cApB_to_C(num_entries, 0.5, dU2.data(), Uhold.data(), solver.Uc.data());
+    // TODO: Add limiter call here? Or somewhere else?
+
+    // printf("--------- STAGE 2 ---------\n");
+    // Kokkos::deep_copy(solver.h_Uc, solver.Uc);
+    // solver.network.print_3d_view(solver.h_Uc);
+    /* ---------------------------------------------- */
+    /*                  Third stage                   */
+    /* ---------------------------------------------- */
+    solver.get_residual();
+    SolverTools::mult_inv_mass_matrix(dt, iMM_elems, solver.res, dU3);
+
+    num_entries = dU3.extent(0) * dU3.extent(1) * dU3.extent(2);
+    Math::cApB_to_C(num_entries, 1.0, dU3.data(), Uhold.data(), solver.Uc.data());
+    // TODO: Add limiter call here? Or somewhere else?    
+
+    // printf("--------- STAGE 3 ---------\n");
+    // Kokkos::deep_copy(solver.h_Uc, solver.Uc);
+    // solver.network.print_3d_view(solver.h_Uc);
+    /* ---------------------------------------------- */
+    /*                 Fourth stage                   */
+    /* ---------------------------------------------- */
+    solver.time += dt / 2.0;
+    solver.get_residual();
+    SolverTools::mult_inv_mass_matrix(dt, iMM_elems, solver.res, dU4);
+
+    num_entries = dU4.extent(0) * dU4.extent(1) * dU4.extent(2);
+    Math::cApB_to_B(num_entries, 2.0, dU3.data(), dU4.data());
+    Math::cApB_to_B(num_entries, 2.0, dU2.data(), dU4.data());
+    Math::cApB_to_B(num_entries, 1.0, dU1.data(), dU4.data());
+    Math::cApB_to_B(num_entries, 1.0/6.0, dU4.data(), Uhold.data());
+    
+    
+    Kokkos::deep_copy(solver.Uc, Uhold);
+
+    // printf("--------- STAGE 4 ---------\n");
+    // Kokkos::deep_copy(solver.h_Uc, solver.Uc);
+    // solver.network.print_3d_view(solver.h_Uc);
+    // TODO: Add limiter call here? Or somewhere else?    
 }
 
 template class StepperBase<2>;
 template class StepperBase<3>;
 template class FE<2>;
 template class FE<3>;
+template class RK4<2>;
+template class RK4<3>;
+
+template class StepperFactory<2>;
+template class StepperFactory<3>;
--- src/numerics/timestepping/stepper.h
+++ src/numerics/timestepping/stepper.h
@@ -15,11 +15,16 @@ template<unsigned dim>
 class StepperBase {
 
 public:
+    virtual inline void set_time_step(rtype time_step) {dt = time_step;}
+    virtual inline void set_final_time(rtype final_time) {tfinal = final_time;}
+    virtual inline rtype get_time_step(){return dt;}
+    virtual inline rtype get_tfinal(){return tfinal;}
     virtual void take_time_step(Solver<dim>& solver);
 
 
 protected:
     rtype dt;
+    rtype tfinal;
 
 };
 
@@ -27,7 +32,50 @@ protected:
 template<unsigned dim>
 class FE : public StepperBase<dim> {
 public:
+    virtual inline void set_time_step(rtype time_step) override {dt = time_step;}
+    virtual inline void set_final_time(rtype final_time) override {tfinal = final_time;}
+    virtual inline rtype get_time_step() override {return dt;}
+    virtual inline rtype get_tfinal() override {return tfinal;}
     virtual void take_time_step(Solver<dim>& solver) override;
+
+protected:
+    rtype dt;
+    rtype tfinal;
 };
+
+template<unsigned dim>
+class RK4 : public StepperBase<dim> {
+public:
+    virtual inline void set_time_step(rtype time_step) override {dt = time_step;}
+    virtual inline void set_final_time(rtype final_time) override {tfinal = final_time;}
+    virtual inline rtype get_time_step() override {return dt;}
+    virtual inline rtype get_tfinal() override {return tfinal;}
+    virtual void take_time_step(Solver<dim>& solver) override;
+
+protected:
+    rtype dt;
+    rtype tfinal;
+};
+
+/*! \brief Factory for stepper object
+ *
+ */
+template<unsigned dim>
+class StepperFactory {
+  public:
+    static StepperBase<dim>* create_stepper(const StepperType type) {
+
+        switch (type) {
+            case StepperType::ForwardEuler:
+                return new FE<dim>();
+            case StepperType::RungeKutta4:
+                return new RK4<dim>();
+            default:
+                throw InputException("StepperFactory could not create a stepper. "
+                                     "The stepper type was not recognized");
+        }
+    }
+};
+
 
 #endif // end DG_NUMERICS_STEPPER_H
